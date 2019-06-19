650dd91727a5611863ec0cec6c9bffa72a38a897
jingteng <jingteng@zk-house.com>
2019-06-19 16:14:04 +0800

    链表：两数相加

---
+++ first-version/src/main/java/com/jt/leetcode/Day20190619Code.java
@@ -0,0 +1,38 @@
+package com.jt.leetcode;
+
+/**
+ * @author: jingteng
+ * @date: 2019/6/19 14:19
+ */
+public class Day20190619Code {
+    /*
+    给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+    如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+    您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+    示例：
+
+    输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+    输出：7 -> 0 -> 8
+    原因：342 + 465 = 807
+    */
+    /**
+     * 解题：卧槽，看不懂题目，下一题
+     * */
+
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+        long freeMemoryBegin = runtime.freeMemory();
+        long begain = System.currentTimeMillis();
+
+        long end = System.currentTimeMillis();
+        long freeMemoryEnd = runtime.freeMemory();
+        System.out.println("总共耗时： "+(end-begain)+" 毫秒");
+        System.out.println("总共耗内存： "+(freeMemoryBegin-freeMemoryEnd));
+
+    }
+
+}

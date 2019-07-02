ba4a7f12cabaa4cd6bcda499f99d6255f462cb4a
jingteng <jingteng@zk-house.com>
2019-07-02 16:57:17 +0800

    有序数组的中位数--初次提交

--- first-version/src/main/java/com/jt/leetcode/Day20190624Code.java
+++ first-version/src/main/java/com/jt/leetcode/Day20190624Code.java
@@ -12,7 +12,7 @@ public class Day20190624Code {
         long freeMemoryBegin = runtime.freeMemory();
         long begain = System.currentTimeMillis();
         //题目4-开始
-        int[] nums1 = new int[]{1,8,15,59,174,178};
+        int[] nums1 = new int[]{1,8};
         int[] nums2 = new int[]{2,5,15,16,17,25,60,257};
         double result = findMedianSortedArrays(nums1,nums2);
         //题目4-结束
@@ -31,28 +31,64 @@ public class Day20190624Code {
         int length2 = nums2.length;
         int result1 = 0;//较小的数
         int result2 = 0;//较大的数
+        int index = 0;//记录当前需要移动指针的数组是1还是2,如果移动数组1的下表指针，那么将index赋值为1，数组2亦然
         while ((index1+index2)<((length1+length2)/2)){
             //找寻中位数：比较当前两个数组的下标所指的元素，比较小的，将指针数+1
             int int1 = nums1[index1];
             int int2 = nums2[index2];
-            //对4个数进行排序，并将最大的两个数给 result1 和 result2，并判断是将哪个指针向后移一位
+            //对4个数进行排序，并将最大的两个数给 result1 和 result2，并判断是将哪个指针向后移一位-------这是错误的思路
+            //纠正：不是将两个最大的数给 result1 和 result2，应该是给数组排序，找出int1和int2谁应该排在result2后面
             if (int1 < int2){
-                if (int1 > result2){//这种情况不可能发生
+                if (index == 1){
+                    //说明数组1是两个连续的数，并且比数组2的当前元素小
+                    result1 = result2;
+                    result2 = int1;
+                }else if (index == 2){
+                    //说明数组2移动下标之后的值比当前数组1的值大
+                    result1 = result2;
+                    result2 = int2;
+                }else {
                     result1 = int1;
                     result2 = int2;
-                }else if (int2 =< result2 && int2 >= result1){
-                result1 = int1;
-                result2 = int2;
-                index1 ++;
+                }
+                //判断当前指针的移动：
+                if (index1 == length1-1){
+                    //数组1已经指到尽头了：只能移动数组2，且数组1不能再参与比较
+                    index2 ++;
+                    index = 2;
+                }else {
+                    index1 ++;
+                    index = 1;
+                }
             }else {
-                result2 = int1;
-                result1 = int2;
-                index2 ++;
+                if (index == 2){
+                    //说明数组2是两个连续的数，并且比数组1的当前元素小
+                    result1 = result2;
+                    result2 = int2;
+                }else if (index == 1){
+                    //说明数组1移动下标之后的值比当前数组2的值大
+                    result1 = result2;
+                    result2 = int1;
+                }else {
+                    result1 = int2;
+                    result2 = int1;
+                }
+                //判断当前指针的移动：
+                if (index2 == length2-1){
+                    //数组1已经指到尽头了：只能移动数组2，且数组1不能再参与比较
+                    index1 ++;
+                    index = 1;
+                }else {
+                    index2 ++;
+                    index = 2;
+                }
             }
         }
         if ((length1+length2) % 2 == 0){//总长度是偶数
             //返回大数和小数的平均数
-            return (result1+result2)/2;
+            double d1 = result1;
+            double d2 = result2;
+            return (d1 + d2)/2;
         }else {
             return result2;
         }
